c977d45a53a03ee06024cc5644c2ad61f9f3ed28
DnzhnAlty <den.altn@hotmail.com>
2022-05-14 23:04:06 +0300

    Game files uploaded

---
+++ Assets/PlaceHolder.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceHolder : MonoBehaviour
+{
+    public GameObject Placer; //üretir kameraya bırakır
+    public Transform CameraPlace;
+    private void Start()
+    {
+       Placer = new GameObject();
+       //Instantiate(Placer, transform.position, Quaternion.identity);
+
+    }
+    public void Hold_The_Place(Transform pos)
+    {
+        Instantiate(Placer,pos);
+    }
+
+    public Transform Access_The_Place()
+    {
+       Transform Old = GameObject.FindWithTag("Placer").GetComponent<GameObject>().transform;
+        return Old;
+    }
+
+    public void Leave_The_Place()
+    {
+        Destroy(Placer);
+    }
+}
